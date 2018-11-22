321ddd78c10ea5480e1da0c19aa0dabd922c608e
morzhanov <vlad.morzhanov@gmail.com>
2018-11-23 00:39:32 +0200

    added content and styles for home component

--- src/components/Dashboard/Home/Home.jsx
+++ src/components/Dashboard/Home/Home.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import './Home.styl'
 
-const Home = () => <div>Home</div>
+const Home = () => (
+  <div className="home">
+    <h1>Welcome to Admin dashboard</h1>
+    <p>Select an entity to start</p>
+  </div>
+)
 
 export default Home
